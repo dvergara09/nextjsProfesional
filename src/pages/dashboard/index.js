7757a5692064477a583b7f985485f5b34b1094a3
Daniel Vergara <danielvsuarez01@gmail.com>
2022-04-23 21:03:53 -0500

    pagination

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,20 +1,39 @@
-const people = [
-  {
-    name: 'Jane Cooper',
-    title: 'Regional Paradigm Technician',
-    department: 'Optimization',
-    role: 'Admin',
-    email: 'jane.cooper@example.com',
-    image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-];
+import { useState } from 'react';
+import useFetch from '@hooks/useFetch';
+import endpoints from '@services/api';
+const LIMIT = 5;
+const OFFSET = 5;
+
+import { usePagination } from '@hooks/usePagination';
+
+import Paginator from '@components/Pagination';
 
 export default function Dashboard() {
+  const [productOffset, setProductOffset] = useState(1);
+  const totalItems = useFetch(endpoints.products.getProducts(0, 0));
+  const pagination = usePagination(LIMIT, totalItems.length, 3);
+  const products = useFetch(endpoints.products.getProducts(LIMIT, productOffset));
+
+  const handlePagination = (event) => {
+    event.preventDefault();
+    let current = event.target.getAttribute('data-page');
+    if (current == null) {
+      current = event.target.parentNode.getAttribute('data-page');
+      if (current == null) {
+        current = event.target.parentNode.parentNode.getAttribute('data-page');
+      }
+    }
+    pagination.setCurrentPage(Number(current));
+    setProductOffset((Number(current) - 1) * PRODUCT_LIMIT);
+  };
+
   return (
     <>
       <div className="flex flex-col">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            {products.length > 0 ? <Paginator pagination={pagination} handleClick={handlePagination} /> : null}
+
             <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
               <table className="min-w-full divide-y divide-gray-200">
                 <thead className="bg-gray-50">
@@ -23,44 +42,50 @@ export default function Dashboard() {
                       Name
                     </th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Title
+                      Category
                     </th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Status
+                      Price
                     </th>
                     <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Role
+                      Id
                     </th>
                     <th scope="col" className="relative px-6 py-3">
                       <span className="sr-only">Edit</span>
                     </th>
+                    <th scope="col" className="relative px-6 py-3">
+                      <span className="sr-only">Delete</span>
+                    </th>
                   </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200">
-                  {people.map((person) => (
-                    <tr key={person.email}>
+                  {products.map((product) => (
+                    <tr key={`Product-item-${product.id}`}>
                       <td className="px-6 py-4 whitespace-nowrap">
                         <div className="flex items-center">
                           <div className="flex-shrink-0 h-10 w-10">
-                            <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+                            <img className="h-10 w-10 rounded-full" src={product.images[0]} alt="" />
                           </div>
                           <div className="ml-4">
-                            <div className="text-sm font-medium text-gray-900">{person.name}</div>
-                            <div className="text-sm text-gray-500">{person.email}</div>
+                            <div className="text-sm font-medium text-gray-900">{product.title}</div>
                           </div>
                         </div>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">{person.title}</div>
-                        <div className="text-sm text-gray-500">{person.department}</div>
+                        <div className="text-sm text-gray-900">{product.category.name}</div>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap">
-                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
+                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">$ {product.price}</span>
                       </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{person.role}</td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.id}</td>
                       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                         <a href="#" className="text-indigo-600 hover:text-indigo-900">
                           Edit
+                        </a>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                        <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                          Delete
                         </a>
                       </td>
                     </tr>
@@ -68,6 +93,7 @@ export default function Dashboard() {
                 </tbody>
               </table>
             </div>
+            {products.length > 0 ? <Paginator pagination={pagination} handleClick={handlePagination} /> : null}
           </div>
         </div>
       </div>
