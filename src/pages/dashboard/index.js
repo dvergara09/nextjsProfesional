33fb4765a414de8bada0d36fbf66acefaf02c855
Daniel Vergara <danielvsuarez01@gmail.com>
2022-05-26 22:09:09 -0500

    chartjs

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import useFetch from '@hooks/useFetch';
 import endPoints from '@services/api/';
-
+import Chart from '@common/Chart';
 import Pagination from '@components/Pagination';
 
 const LIMIT = 5;
@@ -13,8 +13,24 @@ export default function Dashboard() {
   const products = useFetch(endPoints.products.getProducts(LIMIT, offsetProducts), offsetProducts);
   const totalProducts = useFetch(endPoints.products.getProducts(0, 0)).length;
 
+  const categoryNames = products?.map((product) => product.category);
+  const categoryCount = categoryNames?.map((category) => category.name);
+
+  const countOcurrences = (array) => array.reduce((prev, curr) => ((prev[curr] = ++prev[curr] || 1), prev), {});
+
+  const data = {
+    datasets: [
+      {
+        label: 'Categories',
+        data: countOcurrences(categoryCount),
+        borderWidth: 2,
+        backgroundColor: ['#ffbb11', '#c0c0c0', '#50af95', '#23be5f', '#2a71d0'],
+      },
+    ],
+  };
   return (
     <>
+      <Chart className="mb-8 mt-2" chartData={data} />
       <div className="flex flex-col">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
